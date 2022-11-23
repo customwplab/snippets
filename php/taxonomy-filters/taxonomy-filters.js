7500a994d3ed04180d719fdc7300e3b03dd0ca54
customwplab <73247838+customwplab@users.noreply.github.com>
2022-11-23 14:33:08 -0700

    Update taxonomy-filters.js

--- php/taxonomy-filters/taxonomy-filters.js
+++ php/taxonomy-filters/taxonomy-filters.js
@@ -23,4 +23,15 @@ jQuery(function($) {
 		});
 	}
   
+	//Helpers
+	function getFormData($form){
+		var unindexed_array = $form.serializeArray();
+		var indexed_array = {};
+
+		$.map(unindexed_array, function(n, i){
+			indexed_array[n['name']] = n['value'];
+		});
+
+		return indexed_array;
+	}
   	});
